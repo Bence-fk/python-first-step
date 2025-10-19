3135fc8390d4508629475bb2c141253d294d5600
Bence-fk <l.bence99@gmail.com>
2025-10-19 13:32:25 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+print('Hello world!')
+
+# data types 
+
+name = "Bence"
+age =25 
+hieght = 1.81 
+is_student = True
+
+x = 10
+y = 3
+
+#Basic Operations
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+#Strings
+
+text = "Test"
+
+print(text.upper())
+print(text.lower())
+print(text + "3")
+print(text * 3)
+"""
+# git commands for the terminal
+
+
+# git init - inicializáljuk a gitet
+
+# git add . - hozzá adjuk a teljes könyvtárat
