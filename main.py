cd1f6fdeaa3ba2fc3ad7d5e4d5c31927ae55841c
Bence-fk <l.bence99@gmail.com>
2025-10-19 18:41:58 +0200

    git command deleted

--- main.py
+++ main.py
@@ -29,24 +29,3 @@ print(text.lower())
 print(text + "3")
 print(text * 3)
 """
-# git commands for the terminal
-
-
-# git init - inicializáljuk a gitet
-
-# git add . - hozzá adjuk a teljes könyvtárat
-
-# git commit -m "Initial commit"
-
-# git 
-
-# git config --global user.name
-# git config --global user.email
-"""
-push:
-    git add .
-    git commit -m "Describe what changed"
-    git push
-"""
-
-print()
